1cbf241ec9b9b91ca72ae65c7e3506aeb027d1c4
Molly <mojonesycreative@gmail.com>
2022-05-26 13:06:09 -0400

    added handleClick to card cover

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import React from "react";
 import './SingleCard.css';
 
-function SingleCard({ card, cover }) {
+function SingleCard({ key, card, cover, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  }
+
     return (
-        <div className="card">
+        <div className="card" key={key}>
              <div>
                <img className="front" src={card} alt="card front" />
-               <img className="back" src={cover} alt="card back" />
+               <img 
+                className="back" 
+                src={cover} 
+                alt="card back" 
+                onClick={handleClick} 
+                />
              </div>
             </div>
     );
