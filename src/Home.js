156330f42f89055fa0312a60d2982edaf1d286ce
Molly <mojonesycreative@gmail.com>
2022-05-25 21:23:53 -0400

    imported imgs instead of referencing path, mapped imgs

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
+// Images
+import cover from "./cat-photos/match_cat_cover.png";
+import abyssinian from "./cat-photos/abyssinian.jpg";
+import birman from "./cat-photos/birman.jpg"
+import chantilly from "./cat-photos/chantilly.jpg"
+import chartreux from "./cat-photos/chartreux.jpg"
+import devonrex from "./cat-photos/devonrex.png"
+import mainecoon from "./cat-photos/mainecoon.jpg"
+import manx from "./cat-photos/manx.jpg"
+import ragamuffin from "./cat-photos/ragamuffin.jpg"
+import ragdoll from "./cat-photos/ragdoll.jpg"
+import russianblue from "./cat-photos/russianblue.jpg"
 
-const cardImages = [
-    { "src": "./cat-photos/abyssinian.jpg" },
-    { "src": "./cat-photos/birman.jpg" },
-    { "src": "./cat-photos/chantilly.jpg" },
-    { "src": "./cat-photos/chartreux.jpg" },
-    { "src": "./cat-photos/devonrex.jpg" },
-    { "src": "./cat-photos/mainecoon.jpg" },
-    { "src": "./cat-photos/manx.jpg" },
-    { "src": "./cat-photos/ragamuffin.jpg" },
-    { "src": "./cat-photos/ragdoll.jpg" },
-    { "src": "./cat-photos/russianblue.jpg" },
-  ];
+const cardImages = [abyssinian, birman, chantilly, chartreux, devonrex, mainecoon, manx, ragamuffin, ragdoll, russianblue];
 
 function Home() {
   const [cards, setCards] = useState([]);
@@ -21,19 +22,33 @@ function Home() {
   // Shuffle cards
   const shuffleCards = () => {
     const shuffled = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
+      shuffled.sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
 
-      setCards(shuffled);
-      setTurns(0);
+    setCards(shuffled)
+    setTurns(0)
   }
-
+  // Set home view
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
-    <div className="App">
+    <>
       <h1>Match Cats!</h1>
       <button onClick={shuffleCards}>New Game</button>
-    </div>
+    
+      <div className="card-grid">
+        {cards.map(card => (
+            <div className="card" key={card.id}>
+             <div>
+               <img className="front" src={card} alt="card front" />
+               <img className="back" src={cover} alt="card back" />
+             </div>
+            </div>    
+        ))}
+      </div>
+    </>
   );
 }
 
