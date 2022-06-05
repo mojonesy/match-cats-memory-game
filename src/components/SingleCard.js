3ad714b6b512a7cd57cc598282d1f21dc3d0fc69
Molly <mojonesycreative@gmail.com>
2022-06-05 18:28:01 -0400

    remove key prop, add flipped class if flipped is true

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,23 +1,24 @@
 import React from "react";
 import './SingleCard.css';
 
-function SingleCard({ key, card, cover, handleChoice }) {
+function SingleCard({ card, cover, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   }
 
     return (
-        <div className="card" key={key}>
-             <div>
-               <img className="front" src={card} alt="card front" />
-               <img 
-                className="back" 
-                src={cover} 
-                alt="card back" 
-                onClick={handleClick} 
-                />
-             </div>
-            </div>
+        <div className="card">
+        {/* If the flipped prop is set to true, apply the "flipped" class */}
+         <div className={flipped ? "flipped" : ""}>
+           <img className="front" src={card.src} alt="card front" />
+           <img 
+            className="back" 
+            src={cover} 
+            alt="card back" 
+            onClick={handleClick} 
+            />
+         </div>
+        </div>
     );
 }
 
