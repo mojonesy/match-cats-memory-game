6e5869eb4252683387c6f3d64cfd12f5af499057
Molly <mojonesycreative@gmail.com>
2022-06-17 22:50:34 -0400

    add Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Nav() {
+    return (
+      <nav>
+        <ul className="primary-nav flex">
+          <li className="active" />
+            <a href="/">
+              <span aria-hidden="true"></span>Home
+            </a>
+          <li />
+            <a href="/cats">
+              <span aria-hidden="true"></span>About the Cats
+            </a>
+          <li />
+            <a href="https://github.com/mojonesy/match-cats-memory-game">
+              <span aria-hidden="true"></span>Source Code
+            </a>
+        </ul>
+      </nav>
+    );
+}
+
+export default Nav;
