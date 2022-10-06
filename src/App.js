020af3daa730a17f8cc186c3ef2f14286c6f1390
Molly <mojonesycreative@gmail.com>
2022-10-06 00:24:53 -0400

    removed unused div

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <div>
-        <Home />
-      </div>
+
+      <Home />
       
       <div className="footer">
         <footer>
