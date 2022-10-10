fc60da4a992ecbefb5c747266790f58d9393b0ab
Molly <mojonesycreative@gmail.com>
2022-10-09 22:37:02 -0400

    openModal props to Nav, added AboutModal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import AboutModal from './components/AboutModal';
 import Home from './components/Home';
-
+import Nav from './components/Nav';
 
 function App() {
+
+  const openModal = () => {
+    document.body.classList.toggle("modal-open");
+    document.body.classList.toggle("nav-open");
+  }
+
   return (
     <div className="App">
-
+      <Nav openModal={openModal} />
       <Home />
+      <AboutModal />
       
       <div className="footer">
         <footer>
