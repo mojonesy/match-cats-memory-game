175b90445ddfe3ebd037f5ecd13d06b15c6673fd
Molly <mojonesycreative@gmail.com>
2022-10-15 15:07:07 -0400

    import useFetch, pass data to AboutModal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import React from 'react';
+import { useFetch } from './utils/useFetch';
 import './App.css';
 import AboutModal from './components/AboutModal';
 import Home from './components/Home';
 import Nav from './components/Nav';
 
+
 function App() {
+  const { loading, data, error } = useFetch();
+  console.log(data);
 
   const openModal = () => {
     document.body.classList.toggle("modal-open");
     document.body.classList.toggle("nav-open");
   }
 
+  if (loading) return "Loading...";
+  if (error) return `Error: ${error.message}`;
   return (
     <div className="App">
       <Nav openModal={openModal} />
       <Home />
-      <AboutModal />
+      <AboutModal data={data} />
       
       <div className="footer">
         <footer>
