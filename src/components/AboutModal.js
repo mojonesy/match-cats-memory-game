420b0fd5ff2d8f4e03b7716139ecfc299c7e9fa5
Molly <mojonesycreative@gmail.com>
2022-10-15 15:06:48 -0400

    move fetch to useFetch file

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -1,32 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./AboutModal.css";
 import { Icon } from '@iconify/react';
 
-function AboutModal() {
-  const axios = require('axios').default;
-  const VALIDCATS = [
-    'abys', 'birm', 'ctif', 'char', 'drex', 'mcoo', 'manx', 'raga', 'rblu'
-  ];
-
-  const [cats, setCats] = useState([]);
-
-  useEffect(() => {
-    setCats("Loading...");
-    const url = 'https://api.thecatapi.com/v1/breeds';
-    axios.get(url)
-      .then(res => {
-        const data = res.data.filter(cat => VALIDCATS.includes(cat.id));
-        setCats({...data});
-      })
-      .catch(err => console.error(err))
-      .finally(() => console.log(cats));
-  }, []);
+function AboutModal({ data }) {
 
   const closeModal = () => {
     document.body.classList.toggle("modal-open");
   }
 
- 
   return (
     <div className="about-modal">
       <div className="modal-header">
@@ -43,7 +24,7 @@ function AboutModal() {
         </h2>
       </div>
       <div className="carousel">
-        <p>{cats[0].name}</p>
+        <p>{data[0].name}</p>
       </div>
     </div>
   );
