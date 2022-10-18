3e1f1f0b41c7bf101b3d951b077bcfd253afa4e2
Molly <mojonesycreative@gmail.com>
2022-10-17 21:26:14 -0400

    added carousel content html

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -24,7 +24,48 @@ function AboutModal({ data }) {
         </h2>
       </div>
       <div className="carousel">
-        <p>{data[0].name}</p>
+        <img 
+          className="carousel-image" 
+          src={data[0].image.url}
+          alt={data[0].name}
+        />
+
+        <p className="carousel-stat">
+          <span>Name:</span>
+          <span>{data[0].name}</span>
+        </p>
+
+        <p className="carousel-stat">{data[0].description}</p>
+
+        <p className="carousel-stat">
+          <span>Origin:</span>
+          <span>{data[0].origin}</span>
+        </p>
+
+        <p className="carousel-stat">
+          <span>Energy Level:</span>
+          <span>{data[0].energy_level}</span>
+        </p>
+
+        <p className="carousel-stat">
+          <span>Intelligence:</span>
+          <span>{data[0].intelligence}</span>
+        </p>
+
+        <p className="carousel-stat">
+          <span>Shed Level:</span>
+          <span>{data[0].shedding_level}</span>
+        </p>
+
+        <p className="carousel-stat">
+          <span>Life Span:</span>
+          <span>{data[0].life_span} years</span>
+        </p>
+
+        <p className="carousel-stat">
+          <span>Temperament:</span>
+          <span>{data[0].temperament}</span>
+        </p>
       </div>
     </div>
   );
