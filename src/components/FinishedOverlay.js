de8a11f93d66ebc97e5713708fc52759b82f27e9
Molly <mojonesycreative@gmail.com>
2022-11-06 09:19:38 -0500

    final lottie attempt disables button

--- src/components/FinishedOverlay.js
+++ src/components/FinishedOverlay.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
+import Lottie from "lottie-web";
+import confetti from "../assets/confetti.json";
 import "./FinishedOverlay.css";
 
 function FinishedOverlay({ matched, setMatched, shuffleCards }) {
   const [victory, setVictory] = useState(false);
+
+  const play = () => {
+    Lottie.loadAnimation({
+      container: document.querySelector("#confetti-animation"),
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      animationData: confetti
+    });
+  }
 
   useEffect(() => {
     if (matched === 1){
@@ -21,7 +33,7 @@ function FinishedOverlay({ matched, setMatched, shuffleCards }) {
       <>
         {victory &&
           <div id="overlay" className="overlay">
-          <div className="overlay-text">
+          <div className="overlay-text" onMouseOver={play}>
             <h2>Woohoo!</h2>
             <p>You matched the cats! Want to play again?</p>
             <button 
@@ -31,6 +43,10 @@ function FinishedOverlay({ matched, setMatched, shuffleCards }) {
                 Click to restart
             </button>
           </div>
+
+          <div 
+            id="confetti-animation" 
+           />
         </div>}
       </>
     );
