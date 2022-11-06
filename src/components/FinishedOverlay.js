ea0571eae5a6e62ea644749ff4fcbe4bef6e1870
Molly <mojonesycreative@gmail.com>
2022-11-06 07:50:36 -0500

    victory state, useEffect for matched state

--- src/components/FinishedOverlay.js
+++ src/components/FinishedOverlay.js
@@ -1,18 +1,38 @@
+import React, { useEffect, useState } from "react";
 import "./FinishedOverlay.css";
 
-function FinishedOverlay() {
+function FinishedOverlay({ matched, setMatched, shuffleCards }) {
+  const [victory, setVictory] = useState(false);
+
+  useEffect(() => {
+    if (matched === 1){
+      setVictory(true);
+    }
+
+  }, [matched])
+
+  const handleClick = () => {
+    setVictory(false);
+    setMatched(0);
+    shuffleCards();
+  }
+
     return (
-        <div id="overlay" className="overlay">
+      <>
+        {victory &&
+          <div id="overlay" className="overlay">
           <div className="overlay-text">
             <h2>Woohoo!</h2>
             <p>You matched the cats! Want to play again?</p>
             <button 
                 className="overlay-restart-btn"
+                onClick={handleClick}
                 >
                 Click to restart
             </button>
           </div>
-        </div>
+        </div>}
+      </>
     );
 }
 
